0ea57531a597cf550a7cdab850885ba2a349405a
AICHA LAHNITE <halaich300@gmail.com>
2023-03-22 14:01:34 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,15 @@
 
 void print_alphabet_x10(void)
 {
-int count = 0;
-char letter;
+	char i;
+	char h;
 
-while (count++ <= 9)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
-_putchar("\n");
-}
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
