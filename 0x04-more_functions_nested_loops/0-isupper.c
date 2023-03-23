658d961d15dbba2720cd349fe3d3d650df3e66c7
AICHA LAHNITE <halaich300@gmail.com>
2023-03-23 08:13:25 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - Checks for uppercase characters.
- * $c: The character to be checked.
+ * @c: The character to be checked.
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */
