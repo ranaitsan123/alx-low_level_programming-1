4688d6cb0ec047608423abf36acb53ded6821476
AICHA LAHNITE <halaich300@gmail.com>
2023-03-23 15:03:30 -0700

    5. Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ int num, count;
 
 for (count = 0; count <= 9; count++)
 {
-for (num = 0; num <= 14; num++)
+for (num = '0'; num <= '14'; num++)
 {
-if (num > 9)
+if (num > '9')
 _putchar((num / 10) + '0');
 _putchar((num % 10) + '0');
 }
