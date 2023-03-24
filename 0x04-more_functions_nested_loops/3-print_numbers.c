73be20913bbfbcbeeba8034f497a33147bd0778f
AICHA LAHNITE <halaich300@gmail.com>
2023-03-24 14:54:46 -0700

    3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,14 @@
 /**
  * print_numbers - Prints the numbers from 0-9.
  */
+
 void print_numbers(void)
 {
-int i;
+char c;
 
-for (i = 48; i <= 57; i++)
+for (c = '0'; c <= '9'; c++)
 {
-_putchar(i);
+_putchar(c);
 }
 _putchar('\n');
 }
