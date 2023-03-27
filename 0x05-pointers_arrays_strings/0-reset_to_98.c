1219861566e790e71b7789f4c0d8c080162e4298
AICHA LAHNITE <halaich300@gmail.com>
2023-03-27 12:48:51 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include"main.h"
+
+void reset_to_98(int *n){
+/* reset_to_98(): function that takes a pointer to an int as parameter and updates the value it points to to 98
+*/
+int *p=&n;
+*p=98;
+}
