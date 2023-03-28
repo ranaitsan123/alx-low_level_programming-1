9e2f453afee196ad9ee0ce4344bb3c855e0447a0
AICHA LAHNITE <halaich300@gmail.com>
2023-03-28 15:20:21 -0700

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,25 @@
 #include"main.h"
 
 /**
- * puts2 - Prints one char out of two of a string.
- * @str: The string containing characters.
- */
+  * puts2 - Prints every other character of a string
+  * @str: The string to be treated
+  *
+  * Return: void
+  */
 void puts2(char *str)
 {
-	int i, len = 0;
+	int i;
+	int j = 0;
 
-	while (str[i++])
-		len++;
+	while (str[j] != '\0')
+	{
+		j++;
+	}
 
-	for (i = 0; i < len; i += 2)
+	for (i = 0; i < j; i += 2)
+	{
 		_putchar(str[i]);
+	}
 
 	_putchar('\n');
 }
