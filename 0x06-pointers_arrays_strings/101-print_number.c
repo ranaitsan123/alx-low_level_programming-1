09ecb941c0a38daef320d3eb096ab9b4567b091b
AICHA LAHNITE <halaich300@gmail.com>
2023-03-29 14:13:52 -0700

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,44 @@
 #include"main.h"
 
 /**
- * print_number -print number putchar
- * @n:integer
- * Return: void
+ * print_number - prints number
+ * @n:integer to convert to character
+ *
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int abs;
+	int mult = 1;
+	unsigned int abSCount;
+	int i;
+	int c = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		n += 1;
+		n *= -1;
+		n++;
 	}
-	if ((x / 10) > 0)
-		print_number(x / 10);
+	abs = n;
+	abSCount = n;
 
-	_putchar(x % 10 + '0');
+	while (abSCount > 0)
+	{
+		abSCount /= 10;
+		c++;
+	}
+	for (i = 0; i < c - 1; i++)
+		mult *= 10;
+
+	for (i = 0; i < c; i++)
+	{
+		_putchar((abs / mult) + '0');
+		abs = abs % mult;
+		mult /= 10;
+	}
 }
