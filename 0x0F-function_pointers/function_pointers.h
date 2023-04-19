fcc0512ebe8471cc373ab24965a61267bdf5e353
AICHA LAHNITE <halaich300@gmail.com>
2023-04-19 12:47:45 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/*
+ * File: function_pointers.h
+ * AUth: aicha lahnite
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0E-function_pointers directory.
+ */
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
