7db7c86b4801971b8d1de84e7fd2c2db5e59c34b
aicha lahnite <halaicha300@gmail.com>
2023-05-02 21:57:07 +0000

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,55 @@
 /*
  * File: 9-insert_nodeint.c
- * Auth: Brennan D Baraban
+ * Auth: aicha lahnite
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node to a listint_t
- *                           list at a given position.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
- * @idx: The index of the listint_t list where the new
- *       node should be added - indices start at 0.
- * @n: The integer for the new node to contain.
- *
- * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
- */
+  * insert_nodeint_at_index - ...
+  * @head: ...
+  * @idx: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *nouveau, *p = *head;
-	unsigned int nd;
+	listint_t *nouveau, *iter;
+	unsigned int c = 1;
 
-	nouveau = malloc(sizeof(listint_t));
-	if (nouveau == NULL)
-		return (NULL);
-
-	nouveau->n = n;
-
-	if (idx == 0)
+	if (head)
 	{
-		new->next = p;
-		*head = nouveau;
-		return (nouveau);
-	}
-
-	for (nd = 0; nd < (idx - 1); nd++)
-	{
-		if (p == NULL || p->next == NULL)
+		nouveau = malloc(sizeof(listint_t));
+		if (nouveau == NULL)
 			return (NULL);
 
-		p = p->next;
+		nouveau->n = n;
+		if (idx > 0)
+		{
+			iter = *head;
+			while (iter)
+			{
+				if (c == idx)
+				{
+					nouveau->next = iter->next;
+					iter->next = nouveau;
+					return (nouveau);
+				}
+				iter = iter->next;
+				c++;
+			}
+			if (idx > c)
+				return (NULL);
+		}
+		else
+		{
+			nouveau->next = *head;
+			*head = nouveau;
+		}
+		return (nouveau);
 	}
-
-	new->next = p->next;
-	p->next = nouveau;
-
-	return (nouveau);
+	return (NULL);
 }
