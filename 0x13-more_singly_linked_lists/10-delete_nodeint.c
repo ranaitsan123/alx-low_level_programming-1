30e14e98bb65f25a1652c1a34a8ab1d91c63f6a7
aicha lahnite <halaicha300@gmail.com>
2023-05-03 20:30:55 +0000

    chang task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *p = *head;
 	unsigned int nd;
 
-	if (p== NULL)
+	if (p == NULL)
 		return (-1);
 
 	if (index == 0)
