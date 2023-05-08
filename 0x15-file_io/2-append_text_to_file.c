8ab61f09bea499e40feb23672abab75a5a49eaab
aicha lahnite <halaicha300@gmail.com>
2023-05-08 20:22:50 +0000

    task#2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,39 @@
-#include <stdio.h>
+/*
+ * File: 2-append_text_to_file.c
+ * Auth: aicha lahnite
+ */
+
 #include "main.h"
 
 /**
-  * append_text_to_file - ...
-  * @filename: ...
-  * @text_content: ...
-  *
-  * Return: ...
-  */
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: A pointer to the name of the file.
+ * @text_content: The string to add to the end of the file.
+ *
+ * Return: If the function fails or filename is NULL - -1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
+	int file, w, c = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
-	if (text_content)
+	if (text_content != NULL)
 	{
-		if (write(file, text_content, _strlen(text_content)) == -1)
-			return (-1);
+		for (c = 0; text_content[c];)
+			c++;
 	}
 
+	file = open(filename, O_WRONLY | O_APPEND);
+	w = write(file, text_content, c);
+
+	if (file == -1 || w == -1)
+		return (-1);
+
 	close(file);
+
 	return (1);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
