1211d84271b05eba7d400aa535876c0736386bde
aicha lahnite <halaicha300@gmail.com>
2023-05-08 19:59:54 +0000

    task#1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,31 @@
-/*
- * File: 0-read_textfile.c
- * Auth: aicha lahnite
- */
-
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
- */
+  * read_textfile - ...
+  * @filename: The source file
+  * @letters: Number of letters to reads and prints
+  *
+  * Return: ...
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buf;
+	int o, r;
+	char *buf = malloc(sizeof(char *) * letters);
 
-	if (filename == NULL)
+	if (!buf)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	if (!filename)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buf);
+	o = open(filename, O_RDONLY, 0600);
+	if (o == -1)
 		return (0);
-	}
+
+	r = read(o, buf, letters);
+	write(STDOUT_FILENO, buf, r);
 
 	free(buf);
 	close(o);
-
-	return (w);
+	return (r);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,16 +5,20 @@
  * File: main.h
  * Auth: aicha lahnite
  * Desc: Header file containing prototypes for all functions
- *       written in the 0x14-file_io directory.
+ *       written in the 0x15-file_io directory.
  */
 
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 
-#endif /* MAIN_H */
+#endif 
